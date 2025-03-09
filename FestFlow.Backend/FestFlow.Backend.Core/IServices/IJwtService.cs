58d4570e4b8fa16bed32a520b12e04d6a8b7008f
Sarfaraz Ansari <ansarisarfaraz@live.com>
2025-03-09 16:51:40 +0530

    Making project monolithic

---
+++ FestFlow.Backend/FestFlow.Backend.Core/IServices/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using FestFlow.Backend.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestFlow.Backend.Core.IServices
+{
+    public interface IJwtService
+    {
+        Task<string> GenerateToken(User user);
+    }
+}
